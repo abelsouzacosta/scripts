e04397b35c2a15daee81a4a562bb22919a9d0b6b
abelsouzacosta <abelsouzacosta@gmail.com>
2023-10-15 23:12:02 -0300

    [chore]: improve install script

--- install.sh
+++ install.sh
@@ -12,19 +12,28 @@ done < <(find . -maxdepth 1 -type d ! -name "*.*" -print0)
 for subdir in "${subdirectories[@]}"; do
 	absolute_path=$(readlink -f "$subdir")
 	method_file="$absolute_path/main.sh"
-	
+
 	# concatenate the method file into a temporary generated file
 	temporary=$(mktemp)
 
 	echo "Concatenating method to your temporary file [[$temporary]]"
 	cat "$method_file" >> "$temporary"
 
-	echo "Concatenating method to your environment file"
-	cat "$temporary" >> ~/.zshrc
-	
+	function_name=$(basename "$absolute_path")
+
+	if ! grep -q "$function_name" ~/.zshrc; then
+		echo "Adding new script to yout zshrc"
+		echo "" >> ~/.zshrc
+		echo "Concatenating method to your environment file"
+		cat "$temporary" >> ~/.zshrc
+	else
+		echo "The function already exists inside the file"
+	fi
+
 	# remove the temporary file
 	echo "Removing temporary file [[$temporary]]"
 	rm "$temporary"
+
 done
 
 source ~/.zshrc
